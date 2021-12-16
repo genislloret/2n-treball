158014dcae1d5858ffd314b7eaee46febef2aee2
genislloret <96112810+genislloret@users.noreply.github.com>
2021-12-16 10:13:58 +0100

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,29 @@
+input.onLogoEvent(TouchButtonEvent.Pressed, function () {
+    game.addLife(1)
+})
+input.onButtonPressed(Button.A, function () {
+    if (sprite.get(LedSpriteProperty.X) == 2) {
+        game.addScore(1)
+    }
+    if (sprite.get(LedSpriteProperty.X) != 2) {
+        game.gameOver()
+    }
+})
+input.onButtonPressed(Button.B, function () {
+    game.pause()
+    basic.showNumber(game.score())
+    basic.pause(100)
+    game.resume()
+})
+let sprite: game.LedSprite = null
+sprite = game.createSprite(2, 2)
 basic.forever(function () {
-	
+    sprite.move(1)
+    basic.pause(200)
+    if (sprite.isTouchingEdge()) {
+        sprite.ifOnEdgeBounce()
+    }
+    if (sprite.isTouchingEdge()) {
+        sprite.turn(Direction.Right, 60)
+    }
 })
